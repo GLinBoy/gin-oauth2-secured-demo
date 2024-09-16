81c0aaa48f049a00752dd2ac95368df85f37a94b
GLinBoy <glinboy@live.com>
2024-09-16 19:47:49 +0200

    👌 IMPROVE: Add find all book to the BookController

--- controller/book_controller.go
+++ controller/book_controller.go
@@ -1,1 +1,18 @@
 package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/glinboy/gin-oauth2-secured-demo/config"
+	"github.com/glinboy/gin-oauth2-secured-demo/model"
+)
+
+var db = config.DatabaseConnecting()
+
+func FindBooks(ctx *gin.Context) {
+	var books []model.Book
+	db.Find(&books)
+
+	ctx.JSON(http.StatusOK, gin.H{"data": books})
+}
