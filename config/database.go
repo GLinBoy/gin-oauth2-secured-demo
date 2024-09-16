c89b30dc5ec632a28019bc55f67af867ada7dc56
GLinBoy <glinboy@live.com>
2024-09-16 19:39:01 +0200

    👌 IMPROVE: Create Book table

--- config/database.go
+++ config/database.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"github.com/glinboy/gin-oauth2-secured-demo/model"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -9,6 +10,11 @@ func DatabaseConnecting() *gorm.DB {
 	db, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
+	}
+
+	err = db.AutoMigrate(&model.Book{})
+	if err != nil {
+		panic(err)
 	}
 
 	return db
