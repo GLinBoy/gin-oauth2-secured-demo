0d5aa6ce20fefeb35e5bfe8b20a85b9154925b25
GLinBoy <glinboy@live.com>
2024-09-17 20:06:33 +0200

    👌 IMPROVE: Handle books endpoint

--- router/router.go
+++ router/router.go
@@ -1,9 +1,17 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/glinboy/gin-oauth2-secured-demo/controller"
+)
 
 func NewRouter() *gin.Engine {
 	routes := gin.Default()
+
+	apiRoutes := routes.Group("/api")
+
+	bookRoutes := apiRoutes.Group("/books")
+	bookRoutes.GET("", controller.FindBooks)
 
 	return routes
 }
