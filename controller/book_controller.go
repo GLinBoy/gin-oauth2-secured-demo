0b66a4a4aaa38cae15ed7ca054ab53f884a939de
GLinBoy <glinboy@live.com>
2024-09-19 20:27:03 +0200

    👌 IMPROVE: Add create boot endpoint to book controller

--- controller/book_controller.go
+++ controller/book_controller.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/glinboy/gin-oauth2-secured-demo/config"
+	"github.com/glinboy/gin-oauth2-secured-demo/dto"
 	"github.com/glinboy/gin-oauth2-secured-demo/model"
 )
 
@@ -15,4 +16,17 @@ func FindBooks(ctx *gin.Context) {
 	db.Find(&books)
 
 	ctx.JSON(http.StatusOK, gin.H{"data": books})
+}
+
+func CreateBook(ctx *gin.Context) {
+	var bookDTO dto.BookDTO
+	if err := ctx.ShouldBindJSON(&bookDTO); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	book := model.Book{Title: bookDTO.Title, Author: bookDTO.Author}
+	db.Create(&book)
+
+	ctx.JSON(http.StatusCreated, gin.H{"data": book})
 }
